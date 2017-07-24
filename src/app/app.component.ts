c42f26216c886fe3e283ff29b1298814aaecdda1
oxrl <o.romero.lopez@gmail.com>
2017-07-23 22:45:45 -0500

    Proyecto Angular AppNotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = '';
+  title = 'app';
+  my_notes = [
+    {id:1, title: 'Note 1', description: 'Description for note 1'},
+    {id:2, title: 'Note 2', description: 'Description for note 2'},
+    {id:3, title: 'Note 3', description: 'Description for note 3'},
+    {id:4, title: 'Note 4', description: 'Description for note 4'},
+  ];
+  note = {title:null, body:null};
+  show_form = false;
+  addNote(){
+    this.show_form = true;
+  }
+  cancel(){
+    this.show_form = false;
+  }
 }
